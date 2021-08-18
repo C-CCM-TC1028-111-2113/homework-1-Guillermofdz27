2097057e76bd76d0a35538be0efa6d16505833a5
Guillermofdz27 <88850960+Guillermofdz27@users.noreply.github.com>
2021-08-18 17:13:02 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("Dame el numero de paginas")
+    n=float(input())
+    c= float((n//475)*60)
+    x= float(n%475)
+    if x>=1:
+        w=float(c)+60
+        d=float(w)*(.9)
+        print("El costo de la publicación es :" + str(d))
+    else:
+        e=float(c)*(.9)
+        print("El costo de la publicación" +str(c))
+            
     pass
 
 
