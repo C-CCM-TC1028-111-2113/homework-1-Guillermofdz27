cfd9af60c2c692aab19ea754da97d77b7a341e24
Guillermofdz27 <88850960+Guillermofdz27@users.noreply.github.com>
2021-08-18 16:51:42 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -10,7 +10,7 @@ def main():
     c= float(input())
     x= (s)+(i)-(e)-(13*c)
     x1= x-(.075*x)
-    print("El saldo final de la cuenta es: "+str(x1)
+    print("El saldo final de la cuenta es: "+str(x1))
           
     pass
 
